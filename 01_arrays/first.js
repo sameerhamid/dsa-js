27cc09749fa0e99710c72c874cf6fc154ac1f435
sameerhamid <sameerhamid213@gmail.com>
2024-10-01 07:59:45 +0530

    arrays time complexity

---
+++ 01_arrays/first.js
@@ -0,0 +1,24 @@
+const strings = ["a", "b", "c", "d"];
+// 4*4= 16 bytes of storage
+
+// accessing an array of strings
+
+console.log(arr[2]); // O(1)
+
+// push
+strings.push("e"); // O(1)
+// pop
+strings.pop(); // O(1)
+
+// adding item at the beginning
+
+strings.unshift("z"); // O(n)
+
+// removing item at the beginning
+strings.shift(); // O(n)
+
+// adding item at any position
+
+strings.splice(2, 0, "x");
+
+console.log(arr);
